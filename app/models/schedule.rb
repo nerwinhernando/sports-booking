30504f5807d186e54522a7d032027b8fc4f02675
Nerwin Hernando <nerwinc.hernando@gmail.com>
2025-12-22 07:20:42 +0800

    rails generate model Schedule account:references court:references schedule_date:date start_time:time end_time:time price_cents:integer status:string day_type:string is_peak_hour:boolean max_bookings:integer

---
+++ app/models/schedule.rb
@@ -0,0 +1,4 @@
+class Schedule < ApplicationRecord
+  belongs_to :account
+  belongs_to :court
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_21_213638) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_21_231921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -46,6 +46,23 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_21_213638) do
     t.index ["account_id"], name: "index_courts_on_account_id"
     t.index ["venue_id", "court_number"], name: "index_courts_on_venue_id_and_court_number", unique: true
     t.index ["venue_id"], name: "index_courts_on_venue_id"
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.bigint "court_id", null: false
+    t.datetime "created_at", null: false
+    t.string "day_type"
+    t.time "end_time"
+    t.boolean "is_peak_hour"
+    t.integer "max_bookings"
+    t.integer "price_cents"
+    t.date "schedule_date"
+    t.time "start_time"
+    t.string "status"
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_schedules_on_account_id"
+    t.index ["court_id"], name: "index_schedules_on_court_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -104,6 +121,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_21_213638) do
 
   add_foreign_key "courts", "accounts"
   add_foreign_key "courts", "venues"
+  add_foreign_key "schedules", "accounts"
+  add_foreign_key "schedules", "courts"
   add_foreign_key "users", "accounts"
   add_foreign_key "venues", "accounts"
 end
